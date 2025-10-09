ee4186ba095af92806f4ec820e633d6db8376c57
gsk000000 <gabriel.asc980@gmail.com>
2025-10-08 21:50:56 -0300

    a

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -7,3 +7,16 @@ admin.site.register(Device)
 admin.site.register(Measurement)
 admin.site.register(Alert)
 admin.site.register(Organization)
+
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ("device", "value", "measured_at", "organization")
+    list_filter = ("device__organization", "measured_at")
+    search_fields = ("device__name",)
+    ordering = ("-measured_at",)
+    list_select_related = ("device", "organization")
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "zone", "organization")
+    list_filter = ("organization", "category", "zone")
+    search_fields = ("name",)
+    ordering = ("name",)
+    list_select_related = ("category", "zone", "organization")
